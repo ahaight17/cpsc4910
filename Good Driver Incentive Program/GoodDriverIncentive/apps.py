43b8fd8a79520f6f32e04b5e92a0fdd042eabcec
Alex Haight <ahaight@clemson.edu>
2020-05-02 20:44:32 -0400

    cleaning out for .zip upload

--- Good Driver Incentive Program/GoodDriverIncentive/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GooddriverincentiveConfig(AppConfig):
-    name = 'GoodDriverIncentive'
--- hashutils.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def makeHash(password):
-    return hashlib.sha256(str.encode(password)).hexdigest()
-
-def checkHash(password, hash):
-    if makeHash(password) == hash:
-        return True
-    return False
